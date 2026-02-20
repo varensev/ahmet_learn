11887c3ec94a8f6fb10d817b7dd64abaf7f7115a
unknown <lipptren0@gmail.com>
2026-02-20 23:16:37 +0300

    feat: change function expression to arrow function

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const ADD = 'add'
 const MULTI = 'multi'
 const SUBTRACT = 'subtract'
 
-function calc(operation, a, b) {
-  switch (operation) {
-    case ADD:
-      return a + b;
-    case MULTI:
-      return a * b
-    case SUBTRACT:
-      return a - b
-    default:
-      return 'Операция не выбрана'
-  }
+let calc = (operation, a, b) => {      //arrow functions
+    switch (operation) {
+        case ADD:
+            return a + b;
+        case MULTI:
+            return a * b
+        case SUBTRACT:
+            return a - b
+        default:
+            return 'Операция не выбрана'
+    }
 }
 
 const resultMulti = calc(MULTI, 2, 3)
@@ -20,4 +20,3 @@ const resultSubtract = calc(SUBTRACT, 3, 2)
 const resultAdd = calc(ADD, 1, 2)
 
 console.log(`Сложение: ${resultAdd}`, `Вычетание: ${resultSubtract}`, `Умножение: ${resultMulti}`)
-console.log(calc('asd', 2, 3))
