019254328bb109218523fce42cb34114db7224e3
unknown <lipptren0@gmail.com>
2026-02-25 22:03:44 +0300

    feat: to do list wiht array

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
-let numbers = [1, 11, -2, 3, -10, 4];
-const absNumbers = numbers.filter(num => num > 0);
-console.log(`absolute numbers - ${absNumbers}`)
+const STATUS = ({
+    IN_PROGRESS: 'In progress',
+    TO_DO: 'To do',
+    DONE: 'done'
+})
+const PRIORITY = ({
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High'
+})
+const toDoList = [
+    { task: 'eat', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW },
+    { task: 'wake up', status: STATUS.DONE, priority: PRIORITY.MEDIUM },
+    { task: 'swim', status: STATUS.TO_DO, priority: PRIORITY.HIGH },
+    { task: 'create a post', status: STATUS.IN_PROGRESS, priority: PRIORITY.HIGH },
+    { task: 'make a bed', status: STATUS.DONE, priority: PRIORITY.LOW },
+    { task: 'walk', status: STATUS.TO_DO, priority: PRIORITY.MEDIUM },
+
+];
+
+function showList() {
+    console.log(`\tTO DO LIST:`)
+    toDoList.forEach((task, index) => {
+        console.log(`${index + 1}. ${task.task} | ${task.status} | ${task.priority}`)
+    })
+};
+
+function addTask(task) {
+    toDoList.push(task)
+};
+
+function changeTask(index, task) {
+    toDoList[index] = task
+}
+
+function removeTask(taskName) {
+    const index = toDoList.findIndex(x => x.task === taskName)
+    if (index !== -1) {
+        toDoList.splice(index, 1)
+    } else {
+        return false
+    }
+}
 
 
-const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
-const animalOne = animals.find(anim => anim.length >= 5);
-const elephantIndex = animals.indexOf('elephant');
-console.log(`animal - ${animalOne}`);
-console.log(`elephant index - ${elephantIndex}`);
+removeTask('eat')
+
+changeTask(2, { task: 'swim', status: STATUS.IN_PROGRESS, priority: PRIORITY.HIGH })
+
+addTask({ task: 'dasdasd', status: STATUS.IN_PROGRESS, priority: PRIORITY.LOW })
+
+showList()
